50f45ae51527e03bcbe5246acba55a5fffc76b11
1anshu-stack <anshuu424@gmail.com>
2026-01-06 11:32:45 +0530

    Fix: Added validation for create movies api

--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -1,11 +1,26 @@
-import { createMovie, deleteMovie, getMovie, updateMovie, getMovies } from "../controllers/movie.controllers.js";
-import { validateMovieCreateRequest } from "../middleware/movie.middleware.js"
+import { 
+  createMovie, 
+  deleteMovie, 
+  getMovie, 
+  updateMovie, 
+  getMovies 
+} from "../controllers/movie.controllers.js";
 
+import { 
+  validateMovieCreateRequest 
+} from "../middleware/movie.middleware.js"
+
+import {
+  isAuthenticated,
+  isAdminOrClient
+} from "../middleware/user.middleware.js"
 
 const routes = (app) => {
   // routes->function->takes->express->app->object->as->parameter
   app.post(
     '/mba/api/v1/movies', 
+    isAuthenticated,
+    isAdminOrClient,
     validateMovieCreateRequest,
     createMovie
   )
